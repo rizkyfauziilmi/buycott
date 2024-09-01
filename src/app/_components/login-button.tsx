492314fe2a821b1e74a3d6b79545dffd2c000f85
rizkyfauziilmi <rizkyfauziilmi@gmail.com>
2024-09-01 15:08:03 +0700

    refactor(sheet): close sheet on login button
    close the sheet when login button is clicked
    
    fix #9

--- src/app/_components/login-button.tsx
+++ src/app/_components/login-button.tsx
@@ -1,16 +1,25 @@
-"use client"
+"use client";
 
 import { KeyRound } from "lucide-react";
 import { Button } from "~/components/ui/button";
 
 import { useRouter } from "next/navigation";
+import { useSheetStore } from "../stores/use-sheet-store";
 
 export const LoginButton = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const { setIsOpen } = useSheetStore();
 
-    return (
-      <Button className="w-full" variant="outline" onClick={() => router.push("/auth/login")}>
-        <KeyRound className="mr-2 size-4" /> Login Now
-      </Button>
-    );
-}
+  return (
+    <Button
+      className="w-full"
+      variant="outline"
+      onClick={() => {
+        setIsOpen(false);
+        router.push("/auth/login");
+      }}
+    >
+      <KeyRound className="mr-2 size-4" /> Login Now
+    </Button>
+  );
+};
